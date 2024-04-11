c9735d16fdde53a31b0f36d4eba0d637dbaa687c
DUBBRIL\dubbril <dubbril@gmail.com>
2024-04-11 21:38:47 +0700

    Change Aes256CBC To Aes256ECB

--- utils/aes_encrypt.go
+++ utils/aes_encrypt.go
@@ -2,125 +2,128 @@ package utils
 
 import (
 	"crypto/aes"
-	"crypto/cipher"
 	"encoding/base64"
 	"encoding/hex"
-	"errors"
-	"strings"
+	"fmt"
 )
 
-const (
-	ALGORITHM = "AES"
-	CIPHER    = "AES"
-)
-
-func EncryptWithHexKey(text, key string) (string, error) {
-	decodedKey, err := hex.DecodeString(key)
+func EncryptWithBase64Key(plainText, key string) (string, error) {
+	keyBytes, err := base64.StdEncoding.DecodeString(key)
 	if err != nil {
 		return "", err
 	}
 
-	block, err := aes.NewCipher(decodedKey)
+	plainTextBytes := []byte(plainText)
+	block, err := aes.NewCipher(keyBytes)
 	if err != nil {
 		return "", err
 	}
 
-	// Padding the plaintext to a multiple of block size
-	text = addPKCS7Padding(text, block.BlockSize())
-
-	ciphertext := make([]byte, len(text))
-	blockMode := cipher.NewCBCEncrypter(block, decodedKey[:block.BlockSize()])
-	blockMode.CryptBlocks(ciphertext, []byte(text))
-
-	return hex.EncodeToString(ciphertext), nil
-}
-
-func EncryptWithBase64Key(text, key string) (string, error) {
-	decodedKey, err := base64.StdEncoding.DecodeString(key)
-	if err != nil {
-		return "", err
+	// Pad the plaintext if necessary
+	padding := aes.BlockSize - len(plainTextBytes)%aes.BlockSize
+	if padding != 0 {
+		for i := 0; i < padding; i++ {
+			plainTextBytes = append(plainTextBytes, byte(padding))
+		}
 	}
 
-	block, err := aes.NewCipher(decodedKey)
-	if err != nil {
-		return "", err
+	cipherText := make([]byte, len(plainTextBytes))
+	// Encrypt each block separately
+	for bs, be := 0, block.BlockSize(); bs < len(plainTextBytes); bs, be = bs+block.BlockSize(), be+block.BlockSize() {
+		block.Encrypt(cipherText[bs:be], plainTextBytes[bs:be])
 	}
 
-	// Padding the plaintext to a multiple of block size
-	text = addPKCS7Padding(text, block.BlockSize())
-
-	ciphertext := make([]byte, len(text))
-	blockMode := cipher.NewCBCEncrypter(block, decodedKey[:block.BlockSize()])
-	blockMode.CryptBlocks(ciphertext, []byte(text))
-
-	return base64.StdEncoding.EncodeToString(ciphertext), nil
-}
-
-func DecryptWithHexKey(encrypted, key string) (string, error) {
-	decodedKey, err := hex.DecodeString(key)
-	if err != nil {
-		return "", err
-	}
-
-	decodedEncrypted, err := hex.DecodeString(encrypted)
-	if err != nil {
-		return "", err
-	}
-
-	block, err := aes.NewCipher(decodedKey)
-	if err != nil {
-		return "", err
-	}
-
-	if len(decodedEncrypted)%block.BlockSize() != 0 {
-		return "", errors.New("encrypted text is not a multiple of the block size")
-	}
-
-	blockMode := cipher.NewCBCDecrypter(block, decodedKey[:block.BlockSize()])
-	blockMode.CryptBlocks(decodedEncrypted, decodedEncrypted)
-
-	// Remove PKCS7 padding
-	decryptedText := removePKCS7Padding(decodedEncrypted)
-
-	return string(decryptedText), nil
+	return base64.StdEncoding.EncodeToString(cipherText), nil
 }
 
 func DecryptWithBase64Key(encrypted, key string) (string, error) {
-	decodedKey, err := base64.StdEncoding.DecodeString(key)
+	keyBytes, err := base64.StdEncoding.DecodeString(key)
 	if err != nil {
 		return "", err
 	}
 
-	decodedEncrypted, err := base64.StdEncoding.DecodeString(encrypted)
+	encryptedBytes, err := base64.StdEncoding.DecodeString(encrypted)
 	if err != nil {
 		return "", err
 	}
 
-	block, err := aes.NewCipher(decodedKey)
+	block, err := aes.NewCipher(keyBytes)
 	if err != nil {
 		return "", err
 	}
 
-	if len(decodedEncrypted)%block.BlockSize() != 0 {
-		return "", errors.New("encrypted text is not a multiple of the block size")
+	decrypted := make([]byte, len(encryptedBytes))
+	block.Decrypt(decrypted, encryptedBytes)
+
+	plainText, err := removePKCS7Padding(decrypted)
+	if err != nil {
+		return "", err
 	}
 
-	blockMode := cipher.NewCBCDecrypter(block, decodedKey[:block.BlockSize()])
-	blockMode.CryptBlocks(decodedEncrypted, decodedEncrypted)
-
-	// Remove PKCS7 padding
-	decryptedText := removePKCS7Padding(decodedEncrypted)
-
-	return string(decryptedText), nil
+	return string(plainText), nil
 }
 
-func addPKCS7Padding(text string, blockSize int) string {
-	padding := blockSize - (len(text) % blockSize)
-	paddingText := strings.Repeat(string(byte(padding)), padding)
-	return text + paddingText
+func EncryptWithHexKey(plainText, key string) (string, error) {
+	keyBytes, err := hex.DecodeString(key)
+	if err != nil {
+		return "", err
+	}
+
+	plainTextBytes := []byte(plainText)
+
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+
+	// Pad the plaintext if necessary
+	padding := aes.BlockSize - len(plainTextBytes)%aes.BlockSize
+	if padding != 0 {
+		for i := 0; i < padding; i++ {
+			plainTextBytes = append(plainTextBytes, byte(padding))
+		}
+	}
+
+	cipherText := make([]byte, len(plainTextBytes))
+	// Encrypt each block separately
+	for bs, be := 0, block.BlockSize(); bs < len(plainTextBytes); bs, be = bs+block.BlockSize(), be+block.BlockSize() {
+		block.Encrypt(cipherText[bs:be], plainTextBytes[bs:be])
+	}
+
+	return hex.EncodeToString(cipherText), nil
 }
 
-func removePKCS7Padding(text []byte) []byte {
-	padding := int(text[len(text)-1])
-	return text[:len(text)-padding]
+func DecryptWithHexKey(encrypted, key string) (string, error) {
+	keyBytes, err := hex.DecodeString(key)
+	if err != nil {
+		return "", err
+	}
+
+	encryptedBytes, err := hex.DecodeString(encrypted)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+
+	decrypted := make([]byte, len(encryptedBytes))
+	block.Decrypt(decrypted, encryptedBytes)
+
+	plainText, err := removePKCS7Padding(decrypted)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plainText), nil
+}
+
+func removePKCS7Padding(data []byte) ([]byte, error) {
+	padLen := int(data[len(data)-1])
+	if padLen > len(data) || padLen > aes.BlockSize {
+		return nil, fmt.Errorf("invalid padding length")
+	}
+	return data[:len(data)-padLen], nil
 }
